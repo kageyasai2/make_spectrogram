b96b7d27b3f8bb225b51a5830c49afd22774fbfd
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-06-14 11:34:02 +0900

    [add]make Fourier transform graph

--- make_spectrum_graph.py
+++ make_spectrum_graph.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 from pydub import AudioSegment
 
 def exists_music_file_into_directory(music_file_name):
@@ -11,9 +13,29 @@ def input_music_file():
     music_file_name = input('select music file name:')
     exists_music_file_into_directory(music_file_name)
 
+    return music_file_name
+
+def make_graph_music_data(music_file_name):
+    tmp_music_data = AudioSegment.from_mp3(music_file_name)
+    music_datas = np.array(tmp_music_data.get_array_of_samples())
+    print(len(music_datas))
+    music_data = music_datas[:20000]
+    plt.plot(music_data)
+    plt.show()
+
+    return music_data
+
+def make_fourier_transform_graph(music_data):
+    print(music_data)
+    fft = np.fft.fft(music_data)
+    print(type(fft))
+    plt.plot(fft)
+    plt.show()
 
 def main():
-    input_music_file()
+    music_file_name = input_music_file()
+    music_data = make_graph_music_data(music_file_name)
+    make_fourier_transform_graph(music_data)
 
 if __name__ == '__main__':
     main()
